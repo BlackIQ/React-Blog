a236dba8f23a87a3cc3bfde6ce827d1df33e6249
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-07 02:25:51 +0330

    Add and import

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import 'mdb-ui-kit/css/mdb.min.css';
+import 'mdb-ui-kit/js/mdb.min.js';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
