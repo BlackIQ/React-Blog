d73dedc2106984f42686fb0e7bc5e22948a36607
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-07 02:45:35 +0330

    Import blog list

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,37 +1,22 @@
 import {useState} from "react";
+import BlogList from "../components/bloglist";
 
 const Home = () => {
 
-    const [name, setName] = useState('Amir');
-
-    const [blogs, setBlog] = useState([
+    const [blogs] = useState([
         {id: 1, title: 'Laravel', caption: 'What is Laravel?', author: 'Amir', likes: 20},
-        {id: 2, title: 'React', caption: 'What is React?', author: 'Nilo', likes: 10},
+        {id: 1, title: 'React', caption: 'What is React??', author: 'Amir', likes: 25},
+        {id: 2, title: 'Php', caption: 'What is Php?', author: 'Nilo', likes: 10},
         {id: 3, title: 'Js', caption: 'What is Javascript?', author: 'Anon', likes: 15},
     ]);
-
-    const handleClick = (name) => {
-        setName(name);
-    }
 
     return (
         <div className="home">
             <h1>Home page</h1>
             <br />
-            {blogs.map(blog => (
-                <div>
-                    <h4 key={blog.id} className="text-danger">
-                        {blog.title}
-                    </h4>
-                    <p>
-                        <b>{blog.caption}</b> - {blog.author}
-                        <span className="text-danger float-end">{blog.likes} &hearts;</span>
-                    </p>
-
-                </div>
-            ))}
-            {/*<p>{ name }</p>*/}
-            {/*<button onClick={() => handleClick('Hi')} className="btn btn-danger">Click on me</button>*/}
+            <BlogList blogs={blogs} title="All blogs" />
+            <br />
+            <BlogList blogs={blogs.filter(blog => (blog.author === 'Amir'))} title="Amir s blogs" />
         </div>
     );
 }
