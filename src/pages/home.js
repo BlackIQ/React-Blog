ef48f0c5fdfcae44ffb81fee075efa7e1e4980a3
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-07 10:13:37 +0330

    useEffect

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,22 +1,29 @@
-import {useState} from "react";
+import { useState, useEffect } from "react";
 import BlogList from "../components/bloglist";
 
 const Home = () => {
 
-    const [blogs] = useState([
+    const [blogs, setBlogs] = useState([
         {id: 1, title: 'Laravel', caption: 'What is Laravel?', author: 'Amir', likes: 20},
         {id: 1, title: 'React', caption: 'What is React??', author: 'Amir', likes: 25},
         {id: 2, title: 'Php', caption: 'What is Php?', author: 'Nilo', likes: 10},
         {id: 3, title: 'Js', caption: 'What is Javascript?', author: 'Anon', likes: 15},
     ]);
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
+    useEffect(() => {
+        console.log('Use Effect ran');
+    });
+
     return (
         <div className="home">
             <h1>Home page</h1>
-            <br />
-            <BlogList blogs={blogs} title="All blogs" />
-            <br />
-            <BlogList blogs={blogs.filter(blog => (blog.author === 'Amir'))} title="Amir s blogs" />
+            <br/>
+            <BlogList blogs={blogs} handleDelete={handleDelete} title="All blogs"/>
         </div>
     );
 }
