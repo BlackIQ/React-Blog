f24b30dd8bc11e7d0d42d5b1971686f7226ed449
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-07 20:33:49 +0330

    Add link instead of a and href

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return (
         <nav className="navbar bg-light navbar-light">
             <div className="container-fluid">
-                <a className="navbar-brand text-danger">React blog</a>
+                <Link to="/" className="navbar-brand text-danger">React blog</Link>
                 <div className="d-flex align-items-center">
-                    <a className="text-reset me-3" href="/">Home</a>
-                    <a className="text-reset me-3" href="/create">Create</a>
+                    <Link to="/" className="text-reset me-3">Home</Link>
+                    <Link to="/create" className="text-reset me-3">Create</Link>
                 </div>
             </div>
         </nav>
