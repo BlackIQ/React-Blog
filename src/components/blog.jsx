6ae7c3d0fd1bc1a826a1ddc7d0b213d75118fa44
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-08 09:51:47 +0330

    Add likes

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,5 +1,26 @@
+import {FaHeart, FaTrash} from "react-icons/fa";
+import {useHistory} from "react-router-dom";
+
 const SingleBlog = (props) => {
     const blog = props.blog;
+
+    const history = useHistory();
+
+    const handleDelete = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE',
+        }).then(() => {
+            history.push('');
+        });
+    }
+
+    const handleLike = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'UPDATE',
+        }).then(() => {
+            history.push('');
+        });
+    }
 
     return (
         <div>
@@ -7,6 +28,12 @@ const SingleBlog = (props) => {
             <p>Written by <b>{blog.author}</b></p>
             <hr />
             <p>{blog.body}</p>
+            <br />
+            <br />
+            <p className="text-danger">
+                <span onClick={handleDelete} className="pointer"><FaTrash /></span>
+                <span onClick={handleLike} className="pointer float-end">{blog.likes} <FaHeart /></span>
+            </p>
         </div>
     );
 }
