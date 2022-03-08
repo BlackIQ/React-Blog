dab85a71c6e8558c04d2ef05942621d54a190c09
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-08 09:24:10 +0330

    Add send post request and update blog

--- src/pages/create.js
+++ src/pages/create.js
@@ -3,13 +3,34 @@ import {useState} from "react";
 const Create = () => {
     const [title, setTitle] = useState(null);
     const [body, setBody] = useState(null);
-    const [author, setAuthor] = useState('Amir');
+    const [author, setAuthor] = useState('Default');
+
+    const [submitted, setSubmitted] = useState(false);
+    const [isAdding, setAdding] = useState(false);
+
+    const submit = (e) => {
+        e.preventDefault();
+
+        setAdding(true);
+
+        const blog = {title, body, author};
+
+        fetch('http://localhost:8000/blogs/', {
+            method: 'POST',
+            headers: {'Content-Type': 'Application/json'},
+            body: JSON.stringify(blog),
+        }).then(() => {
+            setAdding(false);
+            setSubmitted(true);
+        });
+    }
 
     return (
         <div className="create">
             <h1>Create a new blog</h1>
+            {submitted && <p className="text-success">Blog submitted successfully.</p>}
             <br />
-            <form>
+            <form onSubmit={submit}>
                 <label htmlFor='title' className='form-label'>Blog title</label>
                 <input id='title' className='form-control' placeholder='Blog title' value={title} onChange={(e) => setTitle(e.target.value)} />
                 <br />
@@ -18,12 +39,13 @@ const Create = () => {
                 <br />
                 <label htmlFor='author' className='form-label'>Blog author</label>
                 <select id="author" className="form-select" value={author} onChange={(e) => setAuthor(e.target.value)}>
+                    <option value="Default">Default</option>
                     <option value="Amir">Amir</option>
                     <option value="Nilo">Nilo</option>
                     <option value="Anna">Anna</option>
                 </select>
                 <br />
-                <button className='btn btn-danger'>Add blog</button>
+                { !isAdding ? <button className="btn btn-danger">Add blog</button> : <button className="btn btn-danger" disabled>Adding blog . . .</button>}
             </form>
         </div>
     );
